9c9f957f40804f26da48ff19b54c7d2f5d7b9bb4
Abhijeet Kumar <136921094+LearnerAbhi@users.noreply.github.com>
2026-06-16 17:14:25 +0530

    Time: 0 ms (100.00%) | Memory: 48.6 MB (13.91%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,8 +1,21 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] == target) {
-                return i;
+        // for(int i=0; i<nums.length; i++) {
+        //     if(nums[i] == target) {
+        //         return i;
+        //     }
+        // }
+
+        int si = 0;
+        int ei = nums.length-1;
+        while(si<=ei) {
+            int mid = si + (ei-si)/2;
+            if(target == nums[mid]) {
+                return mid;
+            } else if(target > nums[mid]) {
+                si = mid+1;
+            } else {
+                ei = mid-1;
             }
         }
 
